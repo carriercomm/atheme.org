1ff5fbafcfcb6cd03a754c4988cf5ab769e1c17a
Sam Ferry <sam@atheme.org>
2012-07-11 14:43:07 -0500

    Copy changes on home page.
    Wording change on the home page. Sorry for all of the commits -- a large
    one containing a large number of images, new stylesheets, and a new
    structure for the site files will be coming soon.

--- pages/index.php
+++ pages/index.php
@@ -5,19 +5,12 @@
 	<strong>Recently, Atheme has acquired the popular revision control tracking service, <a href="http://cia.vc" title="CIA">cia.vc</a></strong>
 </p>
 <p>
-	As you can see, we don't have that much listed here on the home page, as there isn't that much to say.
-	Mainly, we develop stuff, so check out the <a href="http://www.atheme.org/projects" title="Projects">projects page</a> for more information.
+	We develop stuff, so check out the <a href="http://www.atheme.org/projects" title="Projects">projects page</a> for more information.
 	For many development versions of the Atheme projects, check out <a href="http://github.com/atheme/" title="Git">our Git repository</a>.
 </p>
 <p>
-	If you have any suggestions on how to improve the website, contact our webmaster via email, or <a href="http://webchat.staticbox.net/?channels=#atheme-project" title="#atheme-project on StaticBox IRC"><span class="italics bold">locate us on IRC via the webchat or at irc.atheme.org</span></a>.
-	Alternatively, if you have an issue or a suggestion, you can open up an issue at <a href="http://github.com/atheme/atheme.org/issues/"><strong>at the bug tracker</strong></a> and it will be seen and taken care of promptly.
-</p>
-<p>
-	If you're interested in tracking the development of the project's website, there is plenty of information to be found on the <a href="http://github.com/atheme/atheme.org/">Git repository</a> for Atheme.org.
-</p>
-<p>
-	Thanks.
+	A new website is coming soon. Have any questions? Contact our webmaster via email, or <a href="http://webchat.staticbox.net/?channels=#atheme-project" title="#atheme-project on StaticBox IRC"><span class="italics bold">locate us on IRC via the webchat or at irc.atheme.org</span></a>.
+	If you have an issue or a suggestion that you would like to "formally" report, open up an issue at <a href="http://github.com/atheme/atheme.org/issues/"><strong>at the bug tracker</strong></a> and it will be seen and taken care of promptly.
 </p>
 
 
