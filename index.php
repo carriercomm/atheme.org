304fa362ef64049b56067c0ce79fc65108cd949f
Sam Ferry <sam@atheme.org>
2012-07-11 14:08:45 -0500

    Font Change
    Trebuchet was ugly. So is Galdeano. Changed it to a smooth Google
    Webfont that is easy on the eyes. Working on a half-redesign for the
    website. Will stay minimalistic in nature but have a little more color
    and a little less shitty. Will revisit other font options then.

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
 <html>
 <head>
 	<title><?php title(); ?></title>
-    <link rel="stylesheet" href="http://www.atheme.org/assets/galdeano.css" type="text/css" media="all" />
 	<link rel="stylesheet" href="http://www.atheme.org/assets/style.css" type="text/css" media="all" />
 	<link rel="stylesheet" href="http://www.atheme.org/assets/additional.css" type="text/css" media="all" />
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 	<meta name="language" content="English">
 	<meta http-equiv="expires" content="never">
 	<meta name="author" content="Sam Ferry">
